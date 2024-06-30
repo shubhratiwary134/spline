2895c557620420c4c340411bf29f6bfbbafe81e5
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-30 20:33:13 +0530

    font changed for the landing page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import '../App.css'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-end bg-[#8C8C8E] p-5 text-black text-2xl border-b-2 border-black'>
+    <div className='flex justify-between items-end bg-[#8C8C8E] p-5 text-black text-2xl border-b-2 border-black navbar-container'>
         <div>Logo</div>
         <div >
-            <ul className=' flex justify-around gap-20'>
+            <ul className=' flex justify-around gap-10 items-end'>
                 <li><a  href='#'>About</a></li>
+               <div className='vl'></div>
                 <li><a href='#'>Products</a></li>
+                <div className='vl'></div>
                 <li><a href='#'>Contact</a></li>
             </ul>
         </div>

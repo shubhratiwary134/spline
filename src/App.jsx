3182a7f1f35ffc1b0e7739589db1106ccd8e60ca
shubhra tiwary <shubhratiwary5@gmail.com>
2024-06-30 15:13:54 +0530

    import spline code snippet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-   <div className='flex m-10 '>
-    <div className='flex w-1/4 '>
-      <h1>experience the power of tech</h1>
-    </div>
-       <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
+   <div className='flex  '> 
+    <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
    </div>
         
    

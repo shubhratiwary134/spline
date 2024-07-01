f37e94c950f920fcca6412e49c1762ce5600def6
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-01 06:20:59 +0530

    updated the spline scene

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
-   <div className='flex flex-col '> 
+    <div>
+       <div className='flex flex-col max-h-screen '> 
    <Navbar></Navbar>
    <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
    </div>
+    </div>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,15 +2,15 @@ import '../App.css'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-end bg-[#8C8C8E] p-5 text-black text-2xl border-b-2 border-black navbar-container'>
+    <div className='flex justify-between  bg-[#8C8C8E] p-2   text-black text-2xl border-b-2 border-black navbar-container'>
         <div>Logo</div>
         <div >
-            <ul className=' flex justify-around gap-10 items-end'>
-                <li><a  href='#'>About</a></li>
-               <div className='vl'></div>
-                <li><a href='#'>Products</a></li>
+            <ul className=' flex justify-around gap-10 '>
+                <li><a  href='#' className='hover:text-white duration-200'>About</a></li>
+               <div className='vl' ></div>
+                <li><a href='#' className='hover:text-white duration-200'>Products</a></li>
                 <div className='vl'></div>
-                <li><a href='#'>Contact</a></li>
+                <li><a href='#' className='hover:text-white duration-200'>Contact</a></li>
             </ul>
         </div>
     </div>

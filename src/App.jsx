51f013ce9b538dfe8cf35f261c75b8708f4c20bd
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-03 00:38:38 +0530

    background image in scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
 import Spline from '@splinetool/react-spline';
-import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
-    <div>
-       <div className='flex flex-col max-h-screen '> 
-   <Navbar></Navbar>
-   <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
-   </div>
+    <div className='h-screen'>
+           <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
     </div>
   );
 }
+
 

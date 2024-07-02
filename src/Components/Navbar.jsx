d60ac3ce086d3f6461bfed33fb027af77b925c83
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-02 07:31:03 +0530

    update in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,14 @@ import '../App.css'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between  bg-[#8C8C8E] p-2   text-black text-2xl border-b-2 border-black navbar-container'>
+    <div className='flex justify-between  bg-[#8C8C8E] p-2   text-black text-2xl navbar-container'>
         <div>Logo</div>
         <div >
             <ul className=' flex justify-around gap-10 '>
                 <li><a  href='#' className='hover:text-white duration-200'>About</a></li>
-               <div className='vl' ></div>
+               {/* <div className='vl' ></div> */}
                 <li><a href='#' className='hover:text-white duration-200'>Products</a></li>
-                <div className='vl'></div>
+                {/* <div className='vl'></div> */}
                 <li><a href='#' className='hover:text-white duration-200'>Contact</a></li>
             </ul>
         </div>

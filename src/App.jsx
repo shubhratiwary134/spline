d359d3460b4b165b5a0fe869a4007c71ff88a870
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-05 06:38:30 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import Navbar from './Components/Navbar';
 
 export default function App() {
   return (
-    <div>
+    <div >
+      <div>
+        <Navbar></Navbar>
+      </div>
        <div className='h-screen'>
-      {/* <Navbar></Navbar> */}
+     
       <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
+    </div>
+    <div className='flex w-full h-72 justify-center items-center'>
+      <p className='text-7xl'>Escape The Rat Race</p>
     </div>
    
     </div>

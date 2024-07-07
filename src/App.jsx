ba3ceff5c872e502e8931af3e7309e6ca09900a2
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-07 17:12:36 +0530

    featured page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Spline from '@splinetool/react-spline';
 import Navbar from './Components/Navbar';
+import Featured from './Components/Featured';
 
 export default function App() {
   return (
-    <div >
+    <div  >
       <div>
         <Navbar></Navbar>
       </div>
-       <div className=' shadow-2xl  '>
+       <div >
        <Spline scene="https://prod.spline.design/9mRzVM9RPXfj6yHY/scene.splinecode" />
     </div>
-    <div className='flex w-full h-72 justify-center items-center'>
-      <p className='text-7xl'>Escape The Rat Race</p>
+    <div className='flex w-full  justify-center items-center'>
+     <Featured></Featured>
     </div>
    
     </div>
---
+++ src/Components/Featured.jsx
@@ -0,0 +1,39 @@
+
+import {motion } from 'framer-motion'
+
+import camera from "../assets/images/camera3.jpg"
+import headphones from "../assets/images/earphone.jpg"
+import drone from "../assets/images/camera.jpg"
+const Featured = () => {
+  return (
+    <div className='w-full flex flex-col mt-12 mx-5 items-center '>
+     <motion.div
+     initial={{}}
+     transition={{repeat:Infinity}}
+     
+     >
+      <motion.p className='text-9xl '>
+      Trending products
+
+      </motion.p>
+      </motion.div>
+      <div className='mt-10 w-full flex flex-col items-center '>
+       
+        <div className='mb-20'>
+          <img src={camera} className='max-h-full hover:scale-105 duration-200'></img>
+        </div>
+        <div className='mb-40'>
+          <img src={headphones} className='max-h-full hover:scale-105 duration-200'></img>
+        </div>
+        <div >
+          <img src={drone} className='max-h-full hover:scale-105 duration-200'></img>
+        </div>
+       
+      </div>
+      
+    
+    </div>
+  )
+}
+
+export default Featured

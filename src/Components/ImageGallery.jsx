9ad0834407ca7e0e466f0f4540b9e18a5542faaa
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-12 11:54:01 +0530

    added framermotion animations to imageScroll

--- src/Components/ImageGallery.jsx
+++ src/Components/ImageGallery.jsx
@@ -1,7 +1,7 @@
 
 import imageCamera from "../assets/images/ImageCamera.jpg"
 import imageCamera2 from "../assets/images/ImageGallery2.jpg"
-import imageDrone from "../assets/images/ImageDrone.jpg"
+import headphoneImage2 from "../assets/images/ImageGalleryHeadphonoes.jpg"
 import headphoneImage from "../assets/images/HeadphoneImage.jpg"
 import { useRef } from "react"
 import { useScroll,motion,useTransform } from "framer-motion"
@@ -11,19 +11,20 @@ const ImageGallery = () => {
     target:ref,
   })
   
-  const x = useTransform(scrollYProgress,[0,1], ["1%","-170%"])
+  const x = useTransform(scrollYProgress,[0,1], ["1%","-190%"])
   return (
     <>
-      <div ref={ref} className='relative h-[700vh] bg-neutral-900'>
-        <div className='sticky top-0 h-screen flex items-center overflow-hidden'>
-           <motion.div style={{x}} className="flex  gap-4 w-[800%]">
-            <img src={imageCamera}  ></img>
-            <img src={imageCamera2} ></img>
-            <img src={imageDrone} ></img>
-            <img src={headphoneImage} ></img>
-            <img src={imageCamera} ></img>
-            <img src={imageCamera2} ></img>
-           
+      <div ref={ref} className=' relative h-[700vh] bg-neutral-900'>
+        <div className='p-10 sticky top-0 h-screen flex   overflow-hidden'>
+           <motion.div style={{x}} className="flex gap-4 ">
+            <img src={imageCamera} className="hover:scale-105 duration-300"></img>
+            <img src={imageCamera2} className="hover:scale-105 duration-300" ></img>
+            <img src={headphoneImage2} className="hover:scale-105 duration-300"></img>
+            <img src={headphoneImage}  className="hover:scale-105 duration-300"></img>
+            <img src={imageCamera}  className="hover:scale-105 duration-300" ></img>
+            <img src={imageCamera2} className="hover:scale-105 duration-300"></img>
+            <img src={imageCamera}   className="hover:scale-105 duration-300"></img>
+            <img src={imageCamera2} className="hover:scale-105 duration-300"></img>
            </motion.div>
         </div>
       </div>
--- src/Components/ImageScroll.jsx
+++ src/Components/ImageScroll.jsx
@@ -4,21 +4,61 @@ import pol1 from "../assets/images/pol1.jpg"
 import pol2 from "../assets/images/pol2.jpg"
 import pol3 from "../assets/images/pol3.jpg"
 import pol4 from "../assets/images/pol4.jpg"
+import { motion } from "framer-motion"
 const ImageScroll = () => {
   return (
    
       <div       
         className=' w-full bg-black flex justify-around items-start  '>
          <div >
-          <img src={pol1} className='max-h-96 rotate-12 '></img>
-          <p className='text-lg text-white'>lorem kamciosdcnsc skc osincscsoi</p>
-          <img src={pol2 } className='max-h-96 -rotate-6'></img>
-          <p>lorem kamciosdcnsc skc osincscsoi</p>
-          <img src={pol4} className='max-h-96 rotate-6  '></img>
-          <img src={pol3} className='max-h-96 rotate-45 '></img>
+          <motion.img
+          initial={{ x: -200, opacity: 0}}
+          whileInView={{ x: 0, opacity: 1, rotate: 12 }}
+          exit={{ x: 200, opacity: 0}}
+          transition={{ duration: 1 }}
+          viewport={{ once: false, amount: 0.5 }}
+          src={pol1}
+          className='max-h-96'></motion.img>
+         
+          <motion.img 
+           initial={{x:-200,opacity:0}}
+           whileInView={{x:0,opacity:1,rotate:-6}}
+           exit={{x:-200,opacity:0}}
+            transition={{duration:1}}
+            viewport={{ once:false,amount:0.5}}
+          src={pol2 } className='max-h-96'></motion.img>
+        
+          <motion.img
+           initial={{x:-200,opacity:0}}
+           whileInView={{x:0,opacity:1,rotate:6}}
+           exit={{x:-200,opacity:0}}
+            transition={{duration:1}}
+            viewport={{
+              once:false,
+              amount:0.5
+            }}
+          src={pol4} className='max-h-96   '></motion.img>
+          <motion.img 
+           initial={{x:-200,opacity:0}}
+           whileInView={{x:0,opacity:1,rotate:45}}
+           exit={{x:-200,opacity:0}}
+            transition={{duration:1}}
+            viewport={{
+              once:false,
+              amount:0.5
+            }}
+          src={pol3} className='max-h-96 z-10 '></motion.img>
          
          </div>
-          <img src={drone} className='sticky top-0 '></img>
+          <motion.img 
+          initial={{x:200}}
+          whileInView={{x:0}}
+          transition={{duration:1}}
+          viewport={{
+            once:false,
+            amount:0.5
+          }}
+          src={drone} className='sticky top-0 '></motion.img>
           
         </div>
 

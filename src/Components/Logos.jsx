14c415f1e22a1346b9b09f32e8ca89023cdb5c7e
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-14 14:04:09 +0530

    logos infinite scroll

--- src/Components/Logos.jsx
+++ src/Components/Logos.jsx
@@ -1,8 +1,10 @@
 
 import { motion } from "framer-motion"
+import {logos,logos2} from "./logo.js"
 const Logos = () => {
+
   return (
-    <div className='bg-zinc-900 w-screen h-[100vh] flex flex-col items-center gap-20 '>
+    <div className='bg-zinc-900 w-screen h-[100vh] flex  flex-col items-center gap-20 '>
         <motion.div 
         initial={{opacity:0,y:20}}
         whileInView={{opacity:1,y:0}}
@@ -12,14 +14,75 @@ const Logos = () => {
           once:false
         }}
         className="flex flex-col items-center mt-10 ">
-            <p className="text-white text-9xl  font-bold"> <span className="text-orange-400">Partners</span></p>
+            <p className="text-white text-8xl  font-bold"> <span className="text-orange-400">Partners</span></p>
         </motion.div>
 
-        
-        <div >
-          <p>hey</p>
-        </div>
+        <motion.div
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:1}}
+        viewport={{
+          amount:0.2,
+          once:false
+        }}
+        className="container overflow-hidden">
+          <div className="flex">
+          <motion.div
+        initial={{x:0}}
+        animate={{x:'-100%'}}
+        transition={{duration:5,repeat:Infinity}}
+        className="flex flex-shrink-0 gap-20" >
+          {logos.map((logo)=>{
+            return (
+              <img key={logo.id} src={logo} className="h-16 w-56"></img>
+            )
+          })}
+        </motion.div>
+        <motion.div
+        initial={{x:0}}
+        animate={{x:'-100%'}}
+        transition={{duration:5,repeat:Infinity}}
+        className="flex flex-shrink-0 gap-20  " >
+          {logos.map((logo)=>{
+            return (
+              <img key={logo.id} src={logo} className="h-16 w-56"></img>
+            )
+          })}
+        </motion.div>
+          </div>
+          </motion.div>
+
+          <div className="container overflow-hidden">
+          <div className="flex">
+          <motion.div
+        initial={{x:0}}
+        animate={{x:'-100%'}}
+        transition={{duration:10,repeat:Infinity}}
+        className="flex flex-shrink-0 gap-20" >
+          {logos2.map((logo)=>{
+            return (
+              <img key={logo.id} src={logo} className="h-16 w-56"></img>
+            )
+          })}
+        </motion.div>
+        <motion.div
+        initial={{x:0}}
+        animate={{x:'-100%'}}
+        transition={{duration:10,repeat:Infinity}}
+        className="flex flex-shrink-0 gap-20  " >
+          {logos2.map((logo)=>{
+            return (
+              <img key={logo.id} src={logo} className="h-16 w-56"></img>
+            )
+          })}
+        </motion.div>
+          </div>
+          </div>
+          
+         
+      
     </div>
+    
   )
 }
 
---
+++ src/Components/logo.js
@@ -0,0 +1,21 @@
+import logo1 from "../logos/01.svg"
+import logo2 from "../logos/02.svg"
+import logo3 from "../logos/03.svg"
+import logo4 from "../logos/04.svg"
+import logo5 from "../logos/05.svg"
+import logo6 from "../logos/06.svg"
+import logo7 from "../logos/07.svg"
+import logo8 from "../logos/08.svg"
+import logo9 from "../logos/09.svg"
+import logo11 from "../logos/11.svg"
+import logo12 from "../logos/12.svg"
+import logo13 from "../logos/13.svg"
+import logo14 from "../logos/14.svg"
+import logo15 from "../logos/15.svg"
+import logo16 from "../logos/16.svg"
+import logo17 from "../logos/17.svg"
+import logo18 from "../logos/18.svg"
+
+ export const logos=[logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo8,logo9]
+ export const logos2 = [logo11,logo12,logo13,logo14,logo15,logo16,logo17,logo18]
+ 

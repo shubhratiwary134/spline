18a1ac9f64046ca0c86a9d8d222c4e689233b507
shubhra tiwary <shubhratiwary5@gmail.com>
2024-07-16 11:56:19 +0530

    footer text changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,24 +4,24 @@ import { SiGmail } from "react-icons/si"
 
 const Footer = () => {
   return (
-    <div className='w-full h-[100vh] bg-white border-t-2 border-black'>
+    <div className='w-full h-[100vh] bg-white border-t-2 px-20 border-black text-5xl'>
       
-      <div className="flex flex-col justify-between">
-        <div className="flex justify-between items-center p-5 text-9xl">
+      <div className="flex flex-col justify-between gap-10">
+        
         <div >Join us</div>
         <div >Quick Links</div>  
        
-        </div>
-        <div className="flex justify-between items-center p-5 text-9xl">
+       
+        
         <div>Service</div>
         <div >Payment </div>  
-        </div>
-        <div className="flex justify-between items-center p-5 text-9xl">
+       
+       
         <div >Subscription</div>
         <div >Contact</div>
         
-        </div>
-        <div className="mt-10 flex  ">
+       
+        <div className="flex  ">
         <FaYoutube size={72}/>
         <BiLogoInstagramAlt size={72} />
         <SiGmail size={72} />
